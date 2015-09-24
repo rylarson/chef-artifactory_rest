8f36cd1072d479361edcc1f5ef0d7f98940f5f91
Ben Jansen <bjansen@tripwire.com>
2015-09-24 08:56:10 -0700

    Add supports declarations

--- metadata.rb
+++ metadata.rb
@@ -5,3 +5,7 @@ license          'MIT'
 description      'Provides resources for interacting with the artifactory rest API'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+
+supports         'windows', '>= 7.0'
+supports         'centos', '>= 5.0'
+supports         'ubuntu', '>= 12.04'

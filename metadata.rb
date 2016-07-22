4e24ebb18c470c001618bb1870f0d29c52af8427
dknacke <dknacke@users.noreply.github.com>
2016-07-22 14:56:24 +0200

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'ryan.mango.larson@gmail.com'
 license          'MIT'
 description      'Provides resources for interacting with the artifactory rest API'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.4.0'
+version          '0.5.0'
 
 supports         'windows', '>= 7.0'
 supports         'centos', '>= 5.0'
